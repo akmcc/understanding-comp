8784980681e18cdf3732afdfc04a4701b1281493
Ashton McCrate <a.mccrate@gmail.com>
2013-12-02 15:15:41 -0500

    add NFA Rulebook and NFA

--- Finite_Automaton.rb
+++ Finite_Automaton.rb
@@ -1,4 +1,5 @@
 #code from Tom Stuart's Understanding Computation
+require 'set'
 
 class FARule < Struct.new(:state, :character, :next_state)
 
@@ -49,6 +50,55 @@ class DFADesign < Struct.new(:start_state, :accept_states, :rulebook)
   end
 
   def accepts?(string)
-    to_dfa.tap {|dfa| dfa.read_string(string) }.accepting?
+    to_dfa.tap {|dfa| dfa.read_string(string)}.accepting?
   end
 end
+
+class NFARulebook < Struct.new(:rules)
+  def next_states(states, character)
+    states.flat_map {|state| follow_rules_for(state, character)}.to_set
+  end
+
+  def follow_rules_for(state, character)
+    rules_for(state, character).map(&:follow)
+  end
+
+  def rules_for(state, character)
+    rules.select {|rule| rule.applies_to?(state, character)}
+  end
+end
+
+
+class NFA < Struct.new(:current_states, :accept_states, :rulebook)
+  def accepting?
+    (current_states & accept_states).any?
+  end
+
+  def read_character(character)
+    self.current_states = rulebook.next_states(current_states, character)
+  end
+
+  def read_string(string)
+    string.chars.each do |character|
+      read_character(character)
+    end
+  end
+end
+
+class NFADesign < Struct.new(:start_state, :accept_states, :rulebook)
+  def accepts?(string)
+    to_nfa.tap {|nfa| nfa.read_string(string) }.accepting?
+  end
+
+  def to_nfa
+    NFA.new(Set[start_state], accept_states, rulebook)
+  end
+end
+
+rulebook = NFARulebook.new([FARule.new(1, 'a', 1), FARule.new(1, 'b', 1), FARule.new(1, 'b', 2), FARule.new(2, 'a', 3), FARule.new(2, 'b', 3), FARule.new(3, 'a', 4), FARule.new(3, 'b', 4)]) 
+
+
+
+
+
+
