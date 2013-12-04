9eb5b328021b9fe4d0543758a31d3f8e162e1bb3
Ashton McCrate <a.mccrate@gmail.com>
2013-12-04 12:06:06 -0500

    add NPDA

--- Pushdown_Automata.rb
+++ Pushdown_Automata.rb
@@ -1,5 +1,7 @@
 # code from Tom Stuart's Understanding Computation
 
+require 'set'
+
 class Stack < Struct.new(:contents)
   
   def push(character)
@@ -20,6 +22,7 @@ class Stack < Struct.new(:contents)
 end
 
 class PDAConfiguration < Struct.new(:state, :stack)
+  
   STUCK_STATE = Object.new
 
   def stuck
@@ -53,6 +56,7 @@ class PDARule < Struct.new(:state, :character, :next_state, :pop_character, :pus
 end
 
 class DPDARulebook < Struct.new(:rules)
+  
   def next_configuration(configuration, character)
     rule_for(configuration, character).follow(configuration)
   end
@@ -75,6 +79,7 @@ class DPDARulebook < Struct.new(:rules)
 end
 
 class DPDA < Struct.new(:current_configuration, :accept_states, :rulebook)
+  
   def accepting?
     accept_states.include?(current_configuration.state)
   end
@@ -106,7 +111,7 @@ class DPDA < Struct.new(:current_configuration, :accept_states, :rulebook)
   end
 end
 
-class DPADesign < Struct.new(:start_state, :bottom_character, :accept_states, :rulebook)
+class DPDADesign < Struct.new(:start_state, :bottom_character, :accept_states, :rulebook)
   
   def accepts?(string)
     to_dpda.tap {|dpda| dpda.read_string(string) }.accepting?
@@ -119,8 +124,66 @@ class DPADesign < Struct.new(:start_state, :bottom_character, :accept_states, :r
   end
 end
 
+class NPDARulebook < Struct.new(:rules)
+  
+  def next_configurations(configurations, character)
+    configurations.flat_map {|config| follow_rules_for(config, character) }.to_set
+  end
 
-rulebook = DPDARulebook.new([PDARule.new(1, '(', 2, '$', ['b', '$']), PDARule.new(2, '(', 2, 'b', ['b', 'b']), PDARule.new(2, ')', 2, 'b', []), PDARule.new(2, nil, 1, '$', ['$'])])
+  def follow_rules_for(configuration, character)
+    rules_for(configuration, character).map {|rule| rule.follow(configuration)}
+  end
+
+  def rules_for(configuration, character)
+    rules.select {|rule| rule.applies_to?(configuration, character) }
+  end
+
+  def follow_free_moves(configurations)
+    more_configurations = next_configurations(configurations, nil)
+    if more_configurations.subset?(configurations)
+      configurations
+    else
+      follow_free_moves(configurations + more_configurations)
+    end
+  end
+end
+
+class NPDA < Struct.new(:current_configurations, :accept_states, :rulebook)
+  
+  def accepting?
+    current_configurations.any? {|config| accept_states.include?(config.state) }
+  end
+
+  def read_character(character)
+    self.current_configurations = rulebook.next_configurations(current_configurations, character)
+  end
+
+  def read_string(string)
+    string.chars.each do |character|
+      read_character(character)
+    end
+  end
+
+  def current_configurations
+    rulebook.follow_free_moves(super)
+  end
+end
+
+class NPDADesign < Struct.new(:start_state, :bottom_character, :accept_states, :rulebook)
+
+  def accepts?(string)
+    to_npda.tap { |npda| npda.read_string(string) }.accepting?
+  end
+
+  def to_npda
+    start_stack = Stack.new([bottom_character])
+    start_configuration = PDAConfiguration.new(start_state, start_stack)
+    NPDA.new(Set[start_configuration], accept_states, rulebook)
+  end
+end
+
+
+rulebook = NPDARulebook.new([PDARule.new(1, 'a', 1, '$', ['a', '$']), PDARule.new(1, 'a', 1, 'a', ['a', 'a']), PDARule.new(1, 'a', 1, 'b', ['a', 'b']), PDARule.new(1, 'b', 1, '$', ['b', '$']), PDARule.new(1, 'b', 1, 'a', ['b', 'a']), PDARule.new(1, 'b', 1, 'b', ['b', 'b']), PDARule.new(1, nil, 2, '$', ['$']), PDARule.new(1, nil, 2, 'a', ['a']), PDARule.new(1, nil, 2, 'b', ['b']), PDARule.new(2, 'a', 2, 'a', []), PDARule.new(2, 'b', 2, 'b', []), PDARule.new(2, nil, 3, '$', ['$'])])
 
 
 
